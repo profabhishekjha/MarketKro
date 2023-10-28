e3cb91170e6e7708915966b56dc4a26ff843d636
profabhishekjha <jhaabhishek910@gmail.com>
2023-10-28 11:56:33 +0530

    2 final cat added

--- src/Pages/Electronics/Electronics.jsx
+++ src/Pages/Electronics/Electronics.jsx
@@ -5,46 +5,88 @@ const Electronics = () => {
   return (
     <div className="mt-10 grid grid-cols-4 items-center justify-center gap-8 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 ">
       <CardSection
-        images={`https://staticimg.titan.co.in/Titan/Catalog/90140QM03_1.jpg?impolicy=pqmed&imwidth=640`}
-        title={`Titan Metal Mechanicals Blue Dial Analog Stainless Steel Strap Watch for Men`}
-        companyName={`TITAN`}
-        description={`Titan Quartz Analog Watch with Blue Colour Strap for Men`}
+        images={`https://m.media-amazon.com/images/I/71gZWUHJqYL._AC_UF350,350_QL80_.jpg`}
+        title={`OnePlus 80 cm (32 inches) Y Series HD Ready LED Smart Android TV 32Y1 (Black)`}
+        companyName={`One Plus`}
+        description={`Treat your eyes to vivid imagery with a high colour range of 93% DCI-P3 featuring a 20% wider colour gamut—authentic cinematic colour with the OnePlus `}
+        price={49999}
+      />
+      <CardSection
+        images={`https://purepng.com/public/uploads/large/purepng.com-refrigeratorrefrigeratorfridgeiceboxrefrigeratoryfreezer-1701528368818jyb9k.png`}
+        title={`LG Double Door Refrigerator`}
+        companyName={`LG`}
+        description={`Closed and Opened Refrigerator Full Of Food and Drinks`}
+        price={12999}
+      />
+      <CardSection
+        images={`https://images.samsung.com/is/image/samsung/assets/in/explore/brand/5-best-android-mobile-phones-2022-in-india/banner-mobile-720x761-080422.jpg?$720_N_JPG$`}
+        title={`Android Phone`}
+        companyName={`Google`}
+        description={`5 Best 5G android mobile phones in India | Samsung India`}
         price={21999}
       />
       <CardSection
-        images={`https://5.imimg.com/data5/ANDROID/Default/2022/2/YY/AY/VF/35336981/product-jpeg.jpg`}
-        title={`Titan Metal Mechanicals Blue Dial Analog Stainless Steel Strap Watch for Men`}
-        companyName={`TITAN`}
-        description={`Titan Quartz Analog Watch with Blue Colour Strap for Men`}
+        images={`https://m.media-amazon.com/images/I/61veIa1VbmL.jpg`}
+        title={`OTG Oven for Baking Cake with 4 Heating Modes`}
+        companyName={`OTG`}
+        description={`LLOT23 Oven, Toaster & Griller, 23 Litres OTG Oven`}
+        price={1800}
+      />
+      <CardSection
+        images={`https://images.samsung.com/is/image/samsung/p6pim/in/ua43t5450akxxl/gallery/in-fhd-t5310-428860-ua43t5450akxxl-532972981?$650_519_PNG$`}
+        title={`OnePlus 80 cm (32 inches) Y Series HD Ready LED Smart Android TV 32Y1 (Black)`}
+        companyName={`One Plus`}
+        description={`Treat your eyes to vivid imagery with a high colour range of 93% DCI-P3 featuring a 20% wider colour gamut—authentic cinematic colour with the OnePlus `}
+        price={49999}
+      />
+      <CardSection
+        images={`https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Dialog_gr_1972.jpg/1200px-Dialog_gr_1972.jpg`}
+        title={`Vintage Phone`}
+        companyName={`ON`}
+        description={`Closed and Opened phone`}
+        price={4999}
+      />
+      <CardSection
+        images={`https://m.media-amazon.com/images/I/71gZWUHJqYL._AC_UF350,350_QL80_.jpg`}
+        title={`OnePlus 80 cm (32 inches) Y Series HD Ready LED Smart Android TV 32Y1 (Black)`}
+        companyName={`One Plus`}
+        description={`Treat your eyes to vivid imagery with a high colour range of 93% DCI-P3 featuring a 20% wider colour gamut—authentic cinematic colour with the OnePlus `}
+        price={49999}
+      />
+      <CardSection
+        images={`https://purepng.com/public/uploads/large/purepng.com-refrigeratorrefrigeratorfridgeiceboxrefrigeratoryfreezer-1701528368818jyb9k.png`}
+        title={`LG Double Door Refrigerator`}
+        companyName={`LG`}
+        description={`Closed and Opened Refrigerator Full Of Food and Drinks`}
+        price={12999}
+      />
+      <CardSection
+        images={`https://images.samsung.com/is/image/samsung/assets/in/explore/brand/5-best-android-mobile-phones-2022-in-india/banner-mobile-720x761-080422.jpg?$720_N_JPG$`}
+        title={`Android Phone`}
+        companyName={`Google`}
+        description={`5 Best 5G android mobile phones in India | Samsung India`}
         price={21999}
       />
       <CardSection
-        images={`https://staticimg.titan.co.in/Titan/Catalog/1825KM01_2.jpg?impolicy=pqmed&imwidth=640`}
-        title={`Titan Metal Mechanicals Blue Dial Analog Stainless Steel Strap Watch for Men`}
-        companyName={`TITAN`}
-        description={`Titan Quartz Analog Watch with Blue Colour Strap for Men`}
-        price={21999}
+        images={`https://m.media-amazon.com/images/I/61veIa1VbmL.jpg`}
+        title={`OTG Oven for Baking Cake with 4 Heating Modes`}
+        companyName={`OTG`}
+        description={`LLOT23 Oven, Toaster & Griller, 23 Litres OTG Oven`}
+        price={1800}
       />
       <CardSection
-        images={`https://staticimg.titan.co.in/Titan/Catalog/90140QM03_1.jpg?impolicy=pqmed&imwidth=640`}
-        title={`Titan Metal Mechanicals Blue Dial Analog Stainless Steel Strap Watch for Men`}
-        companyName={`TITAN`}
-        description={`Titan Quartz Analog Watch with Blue Colour Strap for Men`}
-        price={21999}
+        images={`https://images.samsung.com/is/image/samsung/p6pim/in/ua43t5450akxxl/gallery/in-fhd-t5310-428860-ua43t5450akxxl-532972981?$650_519_PNG$`}
+        title={`OnePlus 80 cm (32 inches) Y Series HD Ready LED Smart Android TV 32Y1 (Black)`}
+        companyName={`One Plus`}
+        description={`Treat your eyes to vivid imagery with a high colour range of 93% DCI-P3 featuring a 20% wider colour gamut—authentic cinematic colour with the OnePlus `}
+        price={49999}
       />
       <CardSection
-        images={`https://staticimg.titan.co.in/Titan/Catalog/90140QM03_1.jpg?impolicy=pqmed&imwidth=640`}
-        title={`Titan Metal Mechanicals Blue Dial Analog Stainless Steel Strap Watch for Men`}
-        companyName={`TITAN`}
-        description={`Titan Quartz Analog Watch with Blue Colour Strap for Men`}
-        price={21999}
-      />
-      <CardSection
-        images={`https://staticimg.titan.co.in/Titan/Catalog/90140QM03_1.jpg?impolicy=pqmed&imwidth=640`}
-        title={`Titan Metal Mechanicals Blue Dial Analog Stainless Steel Strap Watch for Men`}
-        companyName={`TITAN`}
-        description={`Titan Quartz Analog Watch with Blue Colour Strap for Men`}
-        price={21999}
+        images={`https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Dialog_gr_1972.jpg/1200px-Dialog_gr_1972.jpg`}
+        title={`Vintage Phone`}
+        companyName={`ON`}
+        description={`Closed and Opened phone`}
+        price={4999}
       />
     </div>
   )
---
+++ src/Pages/Toys/Toys.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import CardSection from '../../components/cardSectionshop'
+
+const Toys = () => {
+  return (
+    <div className="mt-10 grid grid-cols-4 items-center justify-center gap-8 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 ">
+      <CardSection
+        images={`https://dxkvlfvncvqr8.cloudfront.net/media/file/cms-banner/download_file/relaxo-website-banner-1689137925.png`}
+        title={`Bahamas Flip-Flops for men`}
+        companyName={`Relaxo Footwear`}
+        description={`Best footwear currently`}
+        price={299}
+      />
+      <CardSection
+        images={`https://5.imimg.com/data5/SELLER/Default/2021/1/OW/US/LO/6215968/driving-5--500x500.jpg`}
+        title={`Relex FOOTWEAR Men Leather Driving Shoes`}
+        companyName={`Relex`}
+        description={`Best Solid Fit Black shoes`}
+        price={349}
+      />
+      <CardSection
+        images={`https://imgmedia.lbb.in/media/2020/10/5f869dbb4b344a3ebfad05b9_1602657723903.jpg`}
+        title={`Shop Ethnic Shoes For Men`}
+        companyName={`LLB`}
+        description={`Best footwear brand`}
+        price={299}
+      />
+      <CardSection
+        images={`https://dxkvlfvncvqr8.cloudfront.net/media/file/cms-banner/download_file/relaxo-website-banner-1689137925.png`}
+        title={`Bahamas Flip-Flops for men`}
+        companyName={`Relaxo Footwear`}
+        description={`Best footwear currently`}
+        price={299}
+      />
+      <CardSection
+        images={`https://5.imimg.com/data5/SELLER/Default/2021/1/OW/US/LO/6215968/driving-5--500x500.jpg`}
+        title={`Relex FOOTWEAR Men Leather Driving Shoes`}
+        companyName={`Relex`}
+        description={`Best Solid Fit Black shoes`}
+        price={349}
+      />
+      <CardSection
+        images={`https://imgmedia.lbb.in/media/2020/10/5f869dbb4b344a3ebfad05b9_1602657723903.jpg`}
+        title={`Shop Ethnic Shoes For Men`}
+        companyName={`LLB`}
+        description={`Best footwear brand`}
+        price={299}
+      />
+    </div>
+  )
+}
+
+export default Toys
