951ee3f69029c79a2248e3985036ddfa05c420c7
profabhishekjha <jhaabhishek910@gmail.com>
2023-11-01 12:23:52 +0530

    arranged

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -34,7 +34,6 @@ export default function Home() {
         cards={cards}
         isCardCarousel={true}
       />
-
       <Section
         FirstHeading="Children Products"
         SecondHeading="Toys"
